4f78da3b762f28a07e55aeb8eb745c6bcd8fef58
Christian-Akor <146129512+Christian-Akor@users.noreply.github.com>
2023-12-31 23:45:51 +0100

    Add files via upload

---
+++ web project dec  2023/script.js
@@ -0,0 +1,5 @@
+// You can add interactive features or animations here if desired
+// For a simple greeting, you might not need JavaScript
+function displayMessage() {
+    alert("Merry Christmas and Happy New Year! Wishing you all the joy and success in the world!");
+}
